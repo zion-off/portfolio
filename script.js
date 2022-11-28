4bad4ac08c29de1375889b2578cddee7ad1b1923
zion-off <ajz317@nyu.edu>
2022-11-28 13:14:57 +0400

    portfolio site

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-function showTime() {
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
-    if (h == 0) {
-        h = 12;
-    }
-    if (h > 12) {
-        h = h - 12;
-        session = "PM";
-    }
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
-    setTimeout(showTime, 1000);
+document.addEventListener("DOMContentLoaded", function () {
+    typeWriter();
+});
 
+var i = 0;
+var txt = 'here lies a visual exploratoion of \n my selfhood. peek around -- theres \n things you havent seen before.';
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("name-text").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+    else {
+        commlabfunction();
+    }
 }
-showTime();
 
+var j = 0;
+var commlabtext = 'throughout the semester, ive \n worked on multiple creative \nprojects. this link will guide\n you to everything ive created\n so far. my imagination now\n lives in a cloudspace.';
+var commspeed = 80;
+
+function commlabfunction() {
+    if (j < commlabtext.length) {
+        document.getElementById("commlab-text").innerHTML += commlabtext.charAt(j);
+        j++;
+        setTimeout(typeWriter, commspeed);
+    }
+}
